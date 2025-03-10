7b830964b4032deef5ae7601701c66d004a7ff39
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2025-03-10 11:37:22 -0600

    Update ourViews.sql
    Ben was here

--- ourViews.sql
+++ ourViews.sql
@@ -4,3 +4,17 @@ SELECT recipeID, title, userName
 FROM Recipe
 INNER JOIN User
 ON Recipe.userID = User.userID
+
+-- View Search for recipe
+CREATE VIEW RecipeSearch AS
+SELECT recipeID, title, instructions, notes, userName
+FROM Recipe
+INNER JOIN User ON Recipe.userID = User.userID;
+
+-- View Search for ingredient
+CREATE VIEW IngredientSearch AS
+SELECT i.ingredientID, i.name, u.userName, r.title AS recipeTitle, uI.quantity
+FROM Ingredients i
+INNER JOIN UsedIn uI ON i.ingredientID = uI.ingredientID
+INNER JOIN Recipe r ON uI.recipeID = r.recipeID
+INNER JOIN User u ON r.userID = u.userID;
