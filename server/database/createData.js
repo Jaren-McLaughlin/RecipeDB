12ab66debb8b4e13999f6f0e009cb082bc3a8bd4
unknown <jarenmclaughlin@gmail.com>
2025-04-10 13:05:31 -0600

    118: enhance documentation

--- server/database/createData.js
+++ server/database/createData.js
@@ -16,7 +16,6 @@ const createData = async ({ logging }) => {
     database: process.env.DB_NAME || 'recipeDB',
     multipleStatements: true,
   });
-  
   const createDataPath = path.join(__dirname, 'seeds/insertData.sql');
   const sqlCommands = fs.readFileSync(createDataPath, 'utf8');
   try {
